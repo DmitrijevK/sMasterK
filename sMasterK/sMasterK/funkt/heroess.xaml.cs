984532d35a79f78cf9b85446dfe826076cb195bc
opilane <opilane@HP-CZC6297B9X>
2020-12-07 18:30:23 +0200

    aaa

--- sMasterK/sMasterK/funkt/heroess.xaml.cs
+++ sMasterK/sMasterK/funkt/heroess.xaml.cs
@@ -15,59 +15,13 @@ namespace sMasterK.funkt
     {
         public heroess()
         {
+            InitializeComponent();
 
-            SearchBar searchBar = new SearchBar
-            {
-                Placeholder = "Название персонажа",
-                PlaceholderColor = Color.Black,
-                TextColor = Color.Black,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(SearchBar)),
-                FontAttributes = FontAttributes.Italic
-            };
-
-
-            Title = "Персонажи";
-            StackLayout vtornikv = new StackLayout();
-            string[] taska = new string[] { "А. Ф. Ка", "Ал'акир ", "Алекстраза", "Аранна Звездочет", "Ахалаймахалай", "Вечная Токи" };
-            ListView lista = new ListView();
-
-            lista.ItemsSource = taska;
-
-            lista.ItemSelected += Lista_ItemSelected;
-            Content = new StackLayout { Children = { searchBar, lista } };
-           
         }
 
-        private async void Lista_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+
+        private void aboutlis_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            string text = e.SelectedItem.ToString();
-            if (e.SelectedItemIndex == 0)
-            {
-                await Navigation.PushAsync(new afka());
-            }
-
-            if (e.SelectedItemIndex == 1)
-            {
-                await Navigation.PushAsync(new alakir());
-            }
-
-            if (e.SelectedItemIndex == 2)
-            {
-                await Navigation.PushAsync(new alekstraza());
-            }
-            if (e.SelectedItemIndex == 3)
-            {
-                await Navigation.PushAsync(new arana());
-            }
-            if (e.SelectedItemIndex == 4)
-            {
-                await Navigation.PushAsync(new mahalai());
-            }
-            if (e.SelectedItemIndex == 5)
-            {
-                await Navigation.PushAsync(new toki());
-            }
 
         }
     }
